d504f00aebcaba681004f17946844ab8efd6b3e9
MH_TruongPM <truongpm@mor.com.vn>
2022-05-23 14:45:30 +0700

    fix router contain '#'

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 import TodoScreen from "@/feature/TodoScreen/TodoScreen.vue";
 import LoginScreen from "@/feature/LoginScreen/LoginScreen.vue";
@@ -9,7 +9,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 })
 
